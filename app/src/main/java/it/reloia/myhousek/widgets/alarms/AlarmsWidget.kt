af3316337912ed934570b629bbaccf0e995412d7
ReLoia <zrennyz@gmail.com>
2024-08-14 22:21:38 +0200

    Studied how Glance widget work, made a skeleton for the alarm widget

--- app/src/main/java/it/reloia/myhousek/widgets/alarms/AlarmsWidget.kt
+++ app/src/main/java/it/reloia/myhousek/widgets/alarms/AlarmsWidget.kt
@@ -1,12 +1,26 @@
 package it.reloia.myhousek.widgets.alarms
 
 import android.content.Context
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
+import androidx.glance.LocalContext
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+import androidx.glance.background
+import androidx.glance.layout.Column
+import androidx.glance.layout.Row
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
+import it.reloia.myhousek.R
 
 class AlarmsWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
@@ -20,5 +34,30 @@ class AlarmsWidget : GlanceAppWidget() {
 
 @Composable
 fun WidgetScreen() {
-    Text("Hello, World!")
+    Column (
+        modifier = GlanceModifier
+            .fillMaxSize()
+            .padding(8.dp)
+            .background(color = MaterialTheme.colorScheme.surface)
+    ) {
+        Text(
+            stringResource(R.string.manage_the_alarms),
+            style = TextStyle(
+                fontSize = 38.sp,
+                fontWeight = FontWeight.Bold,
+
+            )
+        )
+//        TODO: automatically load alarms
+        Row {
+//            TODO: remove hardcoded
+            Text("Loading")
+        }
+    }
+}
+
+@Composable
+fun stringResource(id: Int): String {
+    val context = LocalContext.current
+    return context.getString(id)
 }
