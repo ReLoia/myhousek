a6d7443d8dfbb9982d57e4a4733e0973e909b1ca
ReLoia <zrennyz@gmail.com>
2024-08-20 21:55:24 +0200

    Modified the AlarmsWidget

--- app/src/main/java/it/reloia/myhousek/widgets/alarms/AlarmsWidget.kt
+++ app/src/main/java/it/reloia/myhousek/widgets/alarms/AlarmsWidget.kt
@@ -1,11 +1,9 @@
 package it.reloia.myhousek.widgets.alarms
 
 import android.content.Context
-import android.graphics.drawable.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
@@ -13,24 +11,27 @@ import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.ImageProvider
 import androidx.glance.LocalContext
-import androidx.glance.action.Action
 import androidx.glance.action.actionStartActivity
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.components.CircleIconButton
 import androidx.glance.appwidget.components.Scaffold
 import androidx.glance.appwidget.components.TitleBar
+import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
-import androidx.glance.layout.Column
+import androidx.glance.layout.Box
 import androidx.glance.layout.Row
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.height
 import androidx.glance.layout.padding
+import androidx.glance.layout.width
 import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import it.reloia.myhousek.MainActivity
 import it.reloia.myhousek.R
+import it.reloia.myhousek.tasks.domain.model.Task
 import kotlinx.coroutines.launch
 
 class AlarmsWidget : GlanceAppWidget() {
@@ -48,27 +49,10 @@ class AlarmsWidget : GlanceAppWidget() {
 
 @Composable
 fun WidgetScreen(context: Context, id: GlanceId) {
-//    Column (
-//        modifier = GlanceModifier
-//            .fillMaxSize()
-//            .padding(8.dp)
-//            .background(color = MaterialTheme.colorScheme.surface)
-//    ) {
-//        Text(
-//            stringResource(R.string.manage_the_alarms),
-//            style = TextStyle(
-//                fontSize = 38.sp,
-//                fontWeight = FontWeight.Bold,
-//
-//            )
-//        )
-////        TODO: automatically load alarms
-//        Row {
-////            TODO: remove hardcoded
-//            Text("Loading")
-//        }
-//    }
     val coroutineScope = rememberCoroutineScope()
+
+//    TODO: remove boilerplate
+    var alarms: List<Task> = emptyList()
 
     Scaffold (
         titleBar = {
@@ -93,16 +77,69 @@ fun WidgetScreen(context: Context, id: GlanceId) {
         },
         backgroundColor = GlanceTheme.colors.widgetBackground
     ) {
-        Column (
+        val columnCount = 2
+        val rowCount: Int = alarms.size / columnCount
+        LazyColumn (
             modifier = GlanceModifier
                 .fillMaxSize()
-                .padding(8.dp)
+                .background(Color(0xFF000000))
         ) {
-            //TODO: automatically load alarms
-            Row {
-                Text("Loading")
+            println("$rowCount $columnCount")
+//            (0..<rowCount.toInt()).map { c ->
+//                Row (
+//                    modifier = GlanceModifier
+//                        .height(100.dp)
+//                        .padding(8.dp),
+//                ) {
+//                    (0..<columnCount.toInt()).map { r ->
+//                        println("col: ${r + c * columnCount.toInt()}")
+//                        Box (
+//                            modifier = GlanceModifier
+//                                .height(100.dp)
+//                                .width(100.dp)
+//                                .padding(8.dp)
+//                                .background(GlanceTheme.colors.surface)
+//                        ) {
+//                            Text(
+//                                text = "Alarm ${r + c * columnCount.toInt()}",
+//                                style = TextStyle(
+//                                    fontSize = 16.sp,
+//                                    fontWeight = FontWeight.Bold
+//                                )
+//                            )
+//                        }
+//                    }
+//                }
+//            }
+            items(rowCount.toInt()) {
+                Row (
+                    modifier = GlanceModifier
+                        .height(100.dp)
+                        .padding(8.dp),
+                ) {
+                    (0 until columnCount.toInt()).map { r ->
+                        println("col: ${r + it * columnCount.toInt()}")
+                        Box (
+                            modifier = GlanceModifier
+                                .height(100.dp)
+                                .width(100.dp)
+                                .padding(8.dp)
+                                .background(GlanceTheme.colors.surface)
+                        ) {
+                            Text(
+                                text = "Alarm ${r + it * columnCount.toInt()}",
+                                style = TextStyle(
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            )
+                        }
+                    }
+                }
             }
         }
+
+
     }
 }
 
